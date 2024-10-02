28504b1e1ae13abc1ff98fab9b5ad9efe3b67100
SangamChaurasiya <sangam.chaurasiya0604@gmail.com>
2024-10-02 17:10:59 +0530

    Custom user model, Category and Media files

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'category',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'greatkart.wsgi.application'
+
+AUTH_USER_MODEL = 'accounts.Account'
 
 
 # Database
@@ -120,6 +125,12 @@ STATIC_ROOT = BASE_DIR  / 'static'
 STATICFILES_DIRS = [
     'greatkart/static'
 ]
+
+
+# media file configurations
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
