f2fd3d1916e7de489fdb35b1a37333c384a2452b
SangamChaurasiya <sangam.chaurasiya0604@gmail.com>
2024-10-03 19:58:50 +0530

    Cart functionalities and cart item counter with session key

--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
-
+from carts.models import CartItem
+from carts.views import _cart_id
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -25,10 +26,12 @@ def store(request, category_slug=None):
 def product_details(request, category_slug, product_slug):
     try:
         product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=product)
     except Exception as e:
         raise e
 
     context = {
         'product': product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_details.html', context)   
